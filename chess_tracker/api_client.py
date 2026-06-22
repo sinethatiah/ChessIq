6f361b7c580bd9ea28e415d792cbc141a17b01f0
thatiah <thatiahnia@gmail.com>
2026-06-22 09:24:10 +0300

    get player archives from chess api

---
+++ chess_tracker/api_client.py
@@ -0,0 +1,13 @@
+import requests
+
+BASE_URL = "https://api.chess.com/pub/player"
+
+HEADERS = {
+    "User-Agent": "ChessIQ/1.0"
+}
+
+def fetch_archives(username):
+    url=f"{BASE_URL}/{username}/games/archives"
+    response= requests.get(url , headers=HEADERS)
+    data=response.json()
+    return data["archives"]
---
+++ main.py
@@ -0,0 +1,3 @@
+from chess_tracker.api_client import fetch_archives
+archives = fetch_archives("grandlord500")
+print(archives)
