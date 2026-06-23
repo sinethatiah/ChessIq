04bbd67b1ee45f7e8152edc6d8b9d9419adb55c3
thatiah <thatiahnia@gmail.com>
2026-06-23 10:19:36 +0300

    add class functions to handle games

--- chess_tracker/models.py
+++ chess_tracker/models.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 
-class chessGame():
+class ChessGame():
     def __init__(self , raw , username):
         self.username=username.lower()
 
         self.colour= "white" if raw["white"]["username"].lower()==self.username else "black"
         my_side=raw[self.colour]
-        opp_side=raw["black"] if self.colour== "white" else raw["black"]
+        opp_side=raw["black"] if self.colour== "white" else raw["white"]
 
         self.my_rating=my_side["rating"]
         self.opp_rating=opp_side["rating"]
+        self.result=my_side["result"]
 
         eco_url=raw.get("eco", "")
         self.opening=eco_url.split("/")[-1] if eco_url else "unknown"
@@ -18,4 +19,34 @@ class chessGame():
         self.date=datetime.utcfromtimestamp(raw["end_time"])
 
         self.pgn=raw["pgn"]
-    
+        
+
+    def is_win(self):
+            return self.result == "win"
+
+    def is_loss(self):
+            return self.result not in ["win", "agreed", "repetition", "stalemate", "insufficient", "50move"]
+    def was_time_loss(self):
+            return self.result == "timeout"
+    def rating_delta(self, prev_rating):
+            return self.my_rating - prev_rating
+    def outcome(self):
+            if self.result == "win":
+                return "win"
+            elif self.result in ["agreed", "repetition", "stalemate", "insufficient", "50move"]:
+                return "draw"
+            else:
+                return "loss"
+    def to_dict(self):
+            return {
+                "date": self.date.isoformat(),
+                "color": self.colour,
+                "my_rating": self.my_rating,
+                "opp_rating": self.opp_rating,
+                "result": self.result,
+                "outcome": self.outcome(),
+                "opening": self.opening,
+                "time_class": self.time_class,
+                "was_time_loss": self.was_time_loss(),
+            }
+        
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from chess_tracker.api_client import fetch_all_games
+from chess_tracker.models import ChessGame
 
 all_games = fetch_all_games("grandlord500")
 # print(f"Total games fetched: {len(all_games)}")
 import json
-print(json.dumps(all_games[0], indent=2))
+# print(json.dumps(all_games[0], indent=2))
+
+raw_games=fetch_all_games("grandlord500")
+games = [ChessGame (g, "grandlord500") for g in raw_games if g["time_class"]== "rapid"]
+print(f"Rapid games: {len(games)}")
+print(games[0].to_dict())
