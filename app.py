1626f8c834695222bb56529ecb2ad59e5a50eb31
thatiah <thatiahnia@gmail.com>
2026-06-25 04:42:33 +0300

    add title to interface

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ root=tk.Tk()
 root.title("chessIQ")
 root.geometry("900x600")
 
+title_label = tk.Label(root, text="ChessIQ", font=("Arial", 24, "bold"))
+title_label.pack(pady=20)
+
 root.mainloop()
