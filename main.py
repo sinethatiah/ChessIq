e3a7c0b2413d93b3feec021aad3908831396dc34
thatiah <thatiahnia@gmail.com>
2026-06-29 09:18:57 +0300

    comment out the functions used in main.py as it was used for testing

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from chess_tracker.api_client import fetch_all_games
-from chess_tracker.models import ChessGame
-import json
-from chess_tracker.analysis import win_rate_by_colour , win_rate_by_opening, performance_by_hour, rating_over_time, win_rate_by_opponent_gap , streak_tracking , time_trouble_rate
+"""
+This file was used to test the functionalities as the project went on
+"""
+
+
+# from chess_tracker.api_client import fetch_all_games
+# from chess_tracker.models import ChessGame
+# import json
+# from chess_tracker.analysis import win_rate_by_colour , win_rate_by_opening, performance_by_hour, rating_over_time, win_rate_by_opponent_gap , streak_tracking , time_trouble_rate
 
 # all_games = fetch_all_games("grandlord500")
 # print(f"Total games fetched: {len(all_games)}")
 
 # print(json.dumps(all_games[0], indent=2))
 
-raw_games=fetch_all_games("grandlord500")
-games = [ChessGame (g, "grandlord500") for g in raw_games if g["time_class"]== "rapid"]
+# raw_games=fetch_all_games("grandlord500")
+# games = [ChessGame (g, "grandlord500") for g in raw_games if g["time_class"]== "rapid"]
 # print(f"Rapid games: {len(games)}")
 # print(games[0].to_dict())
 
